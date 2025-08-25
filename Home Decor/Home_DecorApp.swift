d120ee89c382b60123f37722bb87694ef5165c77
sugaaaaaa <ra.dalin@lomatechnology.com>
2025-08-25 14:58:33 +0700

    Initial Commit

---
+++ Home Decor/Home_DecorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_DecorApp.swift
+//  Home Decor
+//
+//  Created by Dalynn on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Home_DecorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
